6493c8da38c14c7dc607ed958d05296388cfc72d
JuanMota10 <l22550350@chihuahua2.tecnm.mx>
2025-11-06 19:34:31 -0600

    Add files via upload

---
+++ EVA3_4_EXCEPCIONES_4.sql
@@ -0,0 +1,12 @@
+delimiter $$
+create procedure insertar_prueba(clave int)
+begin
+	declare exit handler for SQLSTATE "23000"
+    begin
+		select "Error: clave duplicada" as mensaje_error;
+    end;
+    
+	insert into prueba value(clave);
+    select "Clave insertada correctamente" as mensaje;
+end$$
+delimiter ;
